7a3ab1bf8939a8b364794f38f934647055e717f9
noor <malaikanoor11786@gmail.com>
2024-03-01 19:33:28 +0500

    google-login
    integrated google account in react.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { jwtDecode } from "jwt-decode";
+import { GoogleLogin } from '@react-oauth/google';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <GoogleOAuthProvider clientId="650148853104-s48bp4teld394dcjono61kl66r84gngd.apps.googleusercontent.com">
+      <GoogleLogin onSuccess={credentialResponse => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+/>
+        
+        
+        </GoogleOAuthProvider>
+      
+    </div>
   )
 }
 
